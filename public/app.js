da5d8dcdad7cc0b8c51772ac996b4e460e1c2fb4
Josh Wood <wood2jp2@gmail.com>
2017-10-02 19:56:42 -0400

    using materialize to append respective cards for each article...

--- public/app.js
+++ public/app.js
@@ -1,6 +1,22 @@
 $.getJSON('/articles', function(data) {
 
-  for (var i = 0; i < data.length; i++) {
-    $('#articles').append(data);
+  for (let i = 0; i < data.length; i++) {
+    // $('#articles').append(data[]);
+
+
+    const currentArticle = data[i];
+    $('#articles').append(`<div class="row">
+    <div class="col s12 m6">\
+            <div class="card blue-grey darken-1">\
+            <div class="card-content white-text">\
+              <span class="card-title">${currentArticle.headline}</span>\
+                <p>${currentArticle.summary}</p>\
+            </div>\
+            <div class="card-action">\
+            <a href="${currentArticle.url}">Check it out!</a>\
+              </div>\
+            </div>\
+          </div>\
+        </div>`);
   }
 })
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const
 
   app.use(express.static("public"));
 
-  mongoose.connect("mongodb://localhost:27017/ScrapingWithMongoTest");
+  mongoose.connect("mongodb://localhost:27017/ScrapingWithMongoTest1");
 
   db.on('error', function(err) {
     console.log('Database Error:', err)
